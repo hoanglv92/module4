8da4ef9799d9e7719b9ac8e4f8461f0f98bfc7fd
hoanglv92 <hoanglv92@gmail.com>
2020-04-11 16:53:15 +0700

    add bt I18n

--- THCustomerApeskl/src/main/java/cg/wbd/grandemonstration/AppConfig.java
+++ THCustomerApeskl/src/main/java/cg/wbd/grandemonstration/AppConfig.java
@@ -8,12 +8,15 @@ import cg.wbd.grandemonstration.service.impl.CustomerServiceImplWithSpringData;
 import cg.wbd.grandemonstration.service.impl.ProvinceServiceImplWithSpringData;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.FactoryBean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.format.Formatter;
@@ -25,9 +28,14 @@ import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.spring4.SpringTemplateEngine;
 import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
@@ -38,7 +46,9 @@ import org.thymeleaf.templateresolver.ITemplateResolver;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
+import java.util.Locale;
 import java.util.Properties;
+import java.util.ResourceBundle;
 
 @Configuration
 @EnableWebMvc
@@ -99,7 +109,7 @@ public class AppConfig extends WebMvcConfigurerAdapter implements ApplicationCon
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/cms");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/cms?characterEncoding=UTF-8");
         dataSource.setUsername("root");
         dataSource.setPassword("123456@Abc");
         return dataSource;
@@ -143,4 +153,24 @@ public class AppConfig extends WebMvcConfigurerAdapter implements ApplicationCon
         Formatter provinceFormatter = new ProvinceFormatter(provinceService);
         registry.addFormatter(provinceFormatter);
     }
+    @Bean
+    public MessageSource messageSource(){
+        ResourceBundleMessageSource messageSource=new ResourceBundleMessageSource();
+        messageSource.setBasename("message");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+    @Override
+    public void addInterceptors(InterceptorRegistry registry){
+        LocaleChangeInterceptor  interceptor=new LocaleChangeInterceptor();
+        interceptor.setParamName("lang");
+        registry.addInterceptor(interceptor);
+    }
+    @Bean
+    public LocaleResolver localeResolver(){
+        SessionLocaleResolver localeResolver=new SessionLocaleResolver();
+        localeResolver.setDefaultLocale(new Locale("en"));
+        return localeResolver;
+    }
+
 }
